9e9ef15863f778ec8f6978a2ee83b90bfd8eb00f
PinelliLuca <p.lucapinelli@gmail.com>
2025-11-21 16:21:16 +0100

    fix: fk user_token

--- src/backend/user/model/user_token.py
+++ src/backend/user/model/user_token.py
@@ -1,10 +1,12 @@
 from src.backend.common.extensions import db
+
 class UserToken(db.Model):
     __tablename__ = "user_token"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
     token = db.Column(db.String, nullable=False)
     issued_at = db.Column(db.DateTime, nullable=False)
     expired_at = db.Column(db.DateTime, nullable=False)
-    user=db.relationship('User', backref=db.backref('user_token', uselist=False))
+
+    user = db.relationship('User', backref=db.backref('user_token', uselist=False))
